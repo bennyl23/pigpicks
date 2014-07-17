f0bd6eede49e0efa517c63ef7acbbd44529ab98a
Ben Lefebvre <bennyl23@yahoo.com>
2014-07-17 16:44:59 -0400

    commit

--- login/admin.py
+++ login/admin.py
@@ -3,7 +3,7 @@ from login.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['user_email', 'user_team_name', 'user_admin_access', 'user_n', 'user_af1', 'user_af2', 'user_paid']
+    list_display = ['user_email', 'user_team_name', 'user_n', 'user_af1', 'user_af2', 'user_paid']
     search_fields = ['user_email', 'user_team_name', 'user_n', 'user_af1', 'user_af2']
     list_filter = ['user_paid']
     list_editable = ['user_paid']
