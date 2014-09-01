4495ddfa875bc7bb91978964983cc7b76cd6c13d
Ben Lefebvre <bennyl23@yahoo.com>
2014-09-01 15:49:09 -0400

    commit

--- login/admin.py
+++ login/admin.py
@@ -14,6 +14,7 @@ class UserAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size':'10'})}
     }
+    list_per_page = 1000
 
 
 class EmailAdmin(admin.ModelAdmin):
